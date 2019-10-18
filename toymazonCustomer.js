5a69b99d06eb6bd5fc5c7809fbe555cb0c600cac
Alex Wan <a_wanderous@hotmail.com>
2019-10-18 19:13:22 +1100

    added how much is wanted to buy

--- toymazonCustomer.js
+++ toymazonCustomer.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var breaker = "-----------------------------"
+var buyProduct;
+var buyQuantity;
+var buyPrice;
+var buyStock;
+
+
 var connection = mysql.createConnection({
     host: process.env.HOST,
     port: 3306,
@@ -10,12 +17,11 @@ var connection = mysql.createConnection({
     database: "toymazon"
 });
 
-var breaker = "-----------------------------"
 connection.connect(function (err) {
     if (err) throw err;
     console.log(breaker)
     console.log("Welcome to Toymazon!")
-    console.log("Your customer id is: " + connection.threadId + "\n");
+    console.log("Your customer id is: " + connection.threadId);
     catalogue();
 });
 
@@ -28,8 +34,39 @@ function catalogue() {
         for (var i = 0; i < res.length; i++) {
             var price = res[i].price
             var priceString = price.toFixed(2);
-            console.log("Item#: " + res[i].id + " |" + " Product Name: " + res[i].product + " |" + " Price: " + "$" + priceString);
+            console.log("Item #: " + res[i].id + " |" + " Product Name: " + res[i].product + " |" + " Price: " + "$" + priceString);
         }
-
+        shopping();
     });
+}
+
+function shopping() {
+    console.log(breaker)
+    inquirer
+        .prompt([{
+            name: "buy",
+            message: "Please enter the item # of the product you wish to purchase.",
+            type: "number"
+        }]).then(function (ans) {
+            itemID = ans.buy;
+            var query1 = "SELECT product, price, stock FROM toymazon.store WHERE ?"
+            connection.query(query1, {id: itemID }, function (err, res){
+                buyProduct = res[0].product;
+                buyStock = res[0].stock;
+                buyProduct = res[0].price;
+                quantity();
+            })
+        })
+}
+
+function quantity(){
+    inquirer
+    .prompt([{
+        name: "quantity",
+        message: "How many of " + buyProduct + "(s) would you like to purchase?",
+        type: "number"
+    }]).then(function (ans){
+        buyQuantity = ans.quantity;
+        enoughStock();
+    }) 
 }
